f243c6f5821cf858c8940a6bde457f4480bb02d9
max-leal <max13leal@gmail.com>
2026-07-30 21:29:17 -0300

    Adicionando a calculadora de IMC

--- CalculadoraIMC/src/CalculadoraIMC.java
+++ CalculadoraIMC/src/CalculadoraIMC.java
@@ -2,15 +2,40 @@ import javax.swing.JOptionPane;
 
 public class CalculadoraIMC {
     public static void main(String[] args) {
-        // TODO: capturar nome, peso, altura
-        // TODO: calcular IMC
-        // TODO: determinar categoria e cor
-        // TODO: exibir com showMessageDialog + HTML
+        try {
+            String nome = JOptionPane.showInputDialog(null, "Digite seu nome:");
+            double peso = Double.parseDouble(JOptionPane.showInputDialog(null, "Digite seu peso em kg:"));
+            double altura = Double.parseDouble(JOptionPane.showInputDialog(null, "Digite sua altura em metros:"));
+
+            double imc = peso / (altura * altura);
+            String[] categoriaCor = getCategoria(imc);
+            String categoria = categoriaCor[0];
+            String cor = categoriaCor[1];
+
+            String html = String.format(
+                "<html><b>%s</b><br>Seu IMC é <font color='%s'><b>%.2f</b></font><br>Categoria: <font color='%s'><b>%s</b></font></html>",
+                nome,
+                cor,
+                imc,
+                cor,
+                categoria
+            );
+
+            JOptionPane.showMessageDialog(null, html, "Resultado do IMC", JOptionPane.INFORMATION_MESSAGE);
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "Digite apenas números válidos!", "Erro", JOptionPane.ERROR_MESSAGE);
+        }
     }
 
     static String[] getCategoria(double imc) {
-        // Retorna [categoria, cor]
-        // TODO: implementar lógica de categorias
-        return new String[]{"", ""};
+        if (imc < 18.5) {
+            return new String[]{"Abaixo do peso", "#3b82f6"};
+        } else if (imc < 25.0) {
+            return new String[]{"Peso normal", "#16a34a"};
+        } else if (imc < 30.0) {
+            return new String[]{"Sobrepeso", "#f97316"};
+        } else {
+            return new String[]{"Obesidade", "#dc2626"};
+        }
     }
 }
