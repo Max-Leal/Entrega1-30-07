d0388ea7afc8e768eec833af60eaf8f1f55f9a9c
max-leal <max13leal@gmail.com>
2026-07-30 21:41:13 -0300

    Add jogo da adivinhacao

--- JogoAdivinhação/src/JogoAdivinhacao.java
+++ JogoAdivinhação/src/JogoAdivinhacao.java
@@ -5,20 +5,66 @@ public class JogoAdivinhacao {
         boolean jogarNovamente = true;
 
         while (jogarNovamente) {
-            int numero = (int)(Math.random() * 100) + 1;
+            int numero = (int) (Math.random() * 100) + 1;
             int tentativas = 0;
+            boolean acertou = false;
 
-            // TODO: loop de adivinhação
-            // TODO: feedback de alto/baixo após cada tentativa
-            // TODO: mensagem de conclusão com performance
-            // TODO: perguntar se quer jogar de novo com showConfirmDialog
+            while (!acertou) {
+                String tentativaTexto = JOptionPane.showInputDialog(null, "Digite um número de 1 a 100:");
 
-            jogarNovamente = false; // remover quando implementar
+                if (tentativaTexto == null) {
+                    JOptionPane.showMessageDialog(null, "Jogo encerrado.");
+                    return;
+                }
+
+                try {
+                    int palpite = Integer.parseInt(tentativaTexto);
+                    tentativas++;
+
+                    if (palpite < 1 || palpite > 100) {
+                        JOptionPane.showMessageDialog(null, "Digite um número entre 1 e 100!");
+                        continue;
+                    }
+
+                    if (palpite < numero) {
+                        JOptionPane.showMessageDialog(null, "Muito baixo!");
+                    } else if (palpite > numero) {
+                        JOptionPane.showMessageDialog(null, "Muito alto!");
+                    } else {
+                        acertou = true;
+                        String classificacao = classificar(tentativas);
+                        JOptionPane.showMessageDialog(
+                            null,
+                            "Acertou!\nVocê acertou em " + tentativas + " tentativas!\n" + classificacao,
+                            "Fim de jogo",
+                            JOptionPane.INFORMATION_MESSAGE
+                        );
+                    }
+                } catch (NumberFormatException e) {
+                    JOptionPane.showMessageDialog(null, "Digite apenas números válidos!");
+                }
+            }
+
+            int resposta = JOptionPane.showConfirmDialog(
+                null,
+                "Deseja jogar novamente?",
+                "Jogar de novo",
+                JOptionPane.YES_NO_OPTION
+            );
+
+            jogarNovamente = (resposta == JOptionPane.YES_OPTION);
         }
     }
 
     static String classificar(int tentativas) {
-        // TODO: retornar classificação baseada no número de tentativas
-        return "";
+        if (tentativas <= 3) {
+            return "🏆 Incrível!";
+        } else if (tentativas <= 6) {
+            return "⭐ Muito bom!";
+        } else if (tentativas <= 10) {
+            return "👍 Bom!";
+        } else {
+            return "💪 Continue praticando!";
+        }
     }
 }
